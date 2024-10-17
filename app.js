a91756686ad198a490d7460e3f1b3745c4eeaa01
elwinspranata <elwinpranata02@gmail.com>
2024-10-17 12:55:45 +0700

    selesai

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const todoRoutes = require('./routes/todo.js')
+const port = 4000;
+
+app.use(express.json());
+
+app.use('/todos', todoRoutes);
+
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.send('elwin!');
 });
 
 app.listen(port, () => {
